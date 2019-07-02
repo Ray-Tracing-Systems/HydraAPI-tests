9db117cba4b07a66d0dd0fbb410628e49a7f44ae
vsan <vsan@protonmail.com>
2019-07-02 18:58:42 +0300

    actual first commit

---
+++ main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <vector>
+//#include <zconf.h>
+
+#include "HydraAPI.h"
+#include "tests.h"
+
+using pugi::xml_node;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////// just leave this it as it is :)
+#include "HydraRenderDriverAPI.h"
+IHRRenderDriver* CreateDriverRTE(const wchar_t* a_cfg) { return nullptr; }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#if defined WIN32
+#include <windows.h> // for SetConsoleCtrlHandler
+#else
+#include <unistd.h>
+#include <signal.h>
+#endif
+
+
+void ErrorCallBack(const wchar_t* message, const wchar_t* callerPlace)
+{
+  std::wcout << callerPlace << L":\t" << message << std::endl;
+}
+
+void InfoCallBack(const wchar_t* message, const wchar_t* callerPlace, HR_SEVERITY_LEVEL a_level)
+{
+  if (a_level >= HR_SEVERITY_WARNING)
+  {
+    if (a_level == HR_SEVERITY_WARNING)
+      std::wcerr << L"WARNING: " << callerPlace << L": " << message; // << std::endl;
+    else      
+      std::wcerr << L"ERROR  : " << callerPlace << L": " << message; // << std::endl;
+  }
+}
+
+
+void destroy()
+{
+  std::cout << "call destroy() --> hrSceneLibraryClose()" << std::endl;
+  hrSceneLibraryClose();
+}
+
+#ifdef WIN32
+BOOL WINAPI HandlerExit(_In_ DWORD fdwControl)
+{
+  exit(0);
+  return TRUE;
+}
+#else
+bool destroyedBySig = false;
+void sig_handler(int signo)
+{
+  if(destroyedBySig)
+    return;
+  switch(signo)
+  {
+    case SIGINT : std::cerr << "\nmain_app, SIGINT";      break;
+    case SIGABRT: std::cerr << "\nmain_app, SIGABRT";     break;
+    case SIGILL : std::cerr << "\nmain_app, SIGINT";      break;
+    case SIGTERM: std::cerr << "\nmain_app, SIGILL";      break;
+    case SIGSEGV: std::cerr << "\nmain_app, SIGSEGV";     break;
+    case SIGFPE : std::cerr << "\nmain_app, SIGFPE";      break;
+    default     : std::cerr << "\nmain_app, SIG_UNKNOWN"; break;
+    break;
+  }
+  std::cerr << " --> hrSceneLibraryClose()" << std::endl;
+  hrSceneLibraryClose();
+  destroyedBySig = true;
+}
+#endif
+
+extern float g_MSEOutput;
+void demo_01_plane_box();
+void demo_02_load_obj();
+void demo_03_caustics();
+void demo_04_instancing();
+
+int main(int argc, const char** argv)
+{
+  hrInfoCallback(&InfoCallBack);
+  hrErrorCallerPlace(L"main");  // for debug needs only
+
+  std::string workingDir = "..";
+  if(argc > 1)
+    workingDir = std::string(argv[1]);
+
+  atexit(&destroy);                           // if application will terminated you have to call hrSceneLibraryClose to free all connections with hydra.exe
+#if defined WIN32
+  SetConsoleCtrlHandler(&HandlerExit, TRUE);  // if some one kill console :)
+  wchar_t NPath[512];
+  GetCurrentDirectoryW(512, NPath);
+#ifdef NEED_DIR_CHANGE
+  SetCurrentDirectoryW(L"../../main");
+#endif
+  std::wcout << L"[main]: curr_dir = " << NPath << std::endl;
+#else
+
+  chdir(workingDir.c_str());
+  char cwd[1024];
+  if (getcwd(cwd, sizeof(cwd)) != nullptr)
+    std::cout << "[main]: curr_dir = " << cwd <<std::endl;
+  else
+    std::cout << "getcwd() error" <<std::endl;
+
+  {
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = sig_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = SA_RESETHAND;
+    sigaction(SIGINT,  &sigIntHandler, NULL);
+    sigaction(SIGSTOP, &sigIntHandler, NULL);
+    sigaction(SIGABRT, &sigIntHandler, NULL);
+    sigaction(SIGILL,  &sigIntHandler, NULL);
+    sigaction(SIGTERM, &sigIntHandler, NULL);
+    sigaction(SIGSEGV, &sigIntHandler, NULL);
+    sigaction(SIGFPE,  &sigIntHandler, NULL);
+  }
+#endif
+  
+  std::cout << "sizeof(size_t) = " << sizeof(size_t) <<std::endl;
+  
+  try
+  {
+    //test42_load_mesh_compressed();
+    
+    run_all_api_tests(); // passed
+    //run_all_geo_tests();
+    //run_all_mtl_tests();
+    //run_all_lgt_tests();
+    //run_all_alg_tests();
+    //run_all_ipp_tests();
+    
+    //window_main_free_look(L"/home/frol/PROG/clsp/database/statex_00001.xml", L"opengl1");
+	  terminate_opengl();
+  }
+  catch (std::runtime_error& e)
+  {
+    std::cout << "std::runtime_error: " << e.what() << std::endl;
+  }
+  catch (...)
+  {
+    std::cout << "unknown exception" << std::endl;
+  }
+
+  hrErrorCallerPlace(L"main"); // for debug needs only
+
+  hrSceneLibraryClose();
+
+  return 0;
+}
---
+++ opengl_init.cpp
@@ -0,0 +1,129 @@
+#if defined(WIN32)
+#define _USE_MATH_DEFINES // Make MS math.h define M_PI
+#endif
+
+#include "tests.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <sstream>
+#include <random>
+
+#include <GLFW/glfw3.h>
+#if defined(WIN32)
+#pragma comment(lib, "glfw3dll.lib")
+#else
+#endif
+
+#include "Timer.h"
+#include "linmath.h"
+
+#include "mesh_utils.h"
+#include "input.h"
+#include "Camera.h"
+
+#include "HydraXMLHelpers.h"
+#include <algorithm>
+using namespace HydraXMLHelpers;
+
+extern Input       g_input;
+static GLFWwindow* g_window = nullptr;
+static int         g_width = 1024;
+static int         g_height = 1024;
+static int            g_filling = 0;
+
+// 
+static void key(GLFWwindow* window, int k, int s, int action, int mods)
+{
+  if (action != GLFW_PRESS) return;
+
+  switch (k)
+  {
+  case GLFW_KEY_Z:
+    //if (mods & GLFW_MOD_SHIFT)
+    //  view_rotz -= 5.0;
+    //else
+    //  view_rotz += 5.0;
+    break;
+  case GLFW_KEY_ESCAPE:
+    glfwSetWindowShouldClose(window, GL_TRUE);
+    break;
+  case GLFW_KEY_UP:
+    //view_rotx += 5.0;
+    break;
+  case GLFW_KEY_DOWN:
+    //view_rotx -= 5.0;
+    break;
+  case GLFW_KEY_LEFT:
+    //view_roty += 5.0;
+    break;
+  case GLFW_KEY_RIGHT:
+    //view_roty -= 5.0;
+    break;
+  case GLFW_KEY_SPACE:
+    if (g_filling == 0)
+    {
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+      g_filling = 1;
+    }
+    else
+    {
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      g_filling = 0;
+    }
+  break;
+  default:
+    return;
+  }
+}
+
+
+void initGLIfNeeded(int a_width, int a_height, const char* a_name)
+{
+  static bool firstCall = true;
+  
+  if (firstCall)
+  {
+    g_width  = a_width;
+    g_height = a_height;
+    
+    if (!glfwInit())
+    {
+      fprintf(stderr, "Failed to initialize GLFW\n");
+      exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+
+    g_window = glfwCreateWindow(g_width, g_height, a_name, NULL, NULL);
+    if (!g_window)
+    {
+      fprintf(stderr, "Failed to open GLFW window\n");
+      glfwTerminate();
+      exit(EXIT_FAILURE);
+    }
+
+    // Set callback functions
+    //glfwSetFramebufferSizeCallback(g_window, reshape);
+    glfwSetKeyCallback(g_window, key);
+
+    glfwMakeContextCurrent(g_window);
+    //gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    glfwGetFramebufferSize(g_window, &g_width, &g_height);
+    //reshape(g_window, g_width, g_height);
+
+    firstCall = false;
+  }
+  else
+  {
+    g_width  = a_width;
+    g_height = a_height;
+  
+    glfwSetWindowSize(g_window, g_width, g_height);
+    glfwSetWindowTitle(g_window, a_name);
+  }
+}
