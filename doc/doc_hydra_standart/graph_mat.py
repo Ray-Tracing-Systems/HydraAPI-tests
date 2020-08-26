62e29cb7f9d27053fa9ba2c76d2524d4a23a8f63
Vladimir Frolov <vova@frolov.pp.ru>
2020-08-26 18:35:29 +0300

    edit

--- doc/doc_hydra_standart/graph_mat.py
+++ doc/doc_hydra_standart/graph_mat.py
@@ -33,25 +33,19 @@ with g.subgraph(name='cluster_3') as c:
 
 
 with g.subgraph(name='cluster_2') as c:
-    c.node('diff', shape = 'diamond', label = 'diff(select)')
-    c.node('refl', shape = 'diamond', label = 'refl(select)')
-    c.node('mix0', label = '''mix(diff, refl)''', shape = 'diamond')
     c.node('mix1', label = '''mix(diff, refl): \nmax_color''')
     c.node('mix2', label = '''mix(diff, refl): \navg_color''')
     c.node('mix3', label = '''mix(diff, refl): \nper_component''', style='dotted')
     c.node('mix4', label = '''mix(diff, refl): \nsum_color''', style='dotted')
-    c.edge('mix0', 'mix1')
-    c.edge('mix0', 'mix2')
-    c.edge('mix0', 'mix3', style='dotted')
-    c.edge('mix0', 'mix4', style='dotted')
+    c.edge('ggx', 'mix1')
+    c.edge('lambert', 'mix1')
+    c.edge('ggx', 'mix2')
+    c.edge('lambert', 'mix2')
+    c.edge('ggx', 'mix3', style='dotted')
+    c.edge('lambert', 'mix3', style='dotted')
+    c.edge('ggx', 'mix4', style='dotted')
+    c.edge('lambert', 'mix4', style='dotted')
     c.attr(label='Mixtures')
-
-g.edge('lambert', 'diff')
-g.edge('orennayar', 'diff', style='dotted')
-g.edge('ggx',     'refl')
-g.edge('mirror',  'refl')
-g.edge('phong',  'refl', style='dotted')
-g.edge('maxphong',  'refl', style='dotted')
 
 g.edge('lambert',  'tex_color')
 g.edge('orennayar',  'tex_color', style='dotted')
@@ -64,10 +58,7 @@ g.edge('ggx',      'tex_gloss')
 g.edge('phong',    'tex_gloss', style='dotted')
 g.edge('maxphong', 'tex_gloss', style='dotted')
 
-g.edge('diff','mix0')
-g.edge('refl','mix0')
-
-g.node('END', shape='doublecircle', label='MAT_LEVEL_1.0')
+g.node('END', shape='Msquare', label='MAT_LEVEL_1.0')
 
 g.edge('mix1','END')
 g.edge('mix2','END')
